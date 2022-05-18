32fa83b77846d324b85b64be4d0d0aa479ac933f
fathanpr <2010631170072@student.unsika.ac.id>
2022-05-18 11:24:22 +0700

    Fathan PR - FrontEnd-Admin

---
+++ resources/views/admin/datapengguna.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<h1>Data Pengguna</h1>
+<hr>
+<div class="datapengguna">
+    <div class="row">
+        <div class="d-flex justify-content-end">
+            <div class="col-lg-4">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Cari Pengguna..." aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <button class="btn btn-success" type="button" id="button-addon2"><i class='bx bx-search-alt-2 icon'></i></button>
+                  </div>
+            </div>
+        </div>
+    </div>
+    <div class="table">
+        <div class="table_header">
+            <div>
+
+            </div>
+        </div>
+        <div class="table_section">
+            <table class=" table scroll table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nama</th>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>Nomor HP</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Fathan</td>
+                        <td>fathan17</td>
+                        <td>fathan@student.unsika.ac.id</td>
+                        <td>08987335266</td>
+                        <td>
+                            <button type="button" class="btn btn-danger">Hapus</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+@endsection
---
+++ resources/views/admin/pengajuan.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<h1>Pengajuan</h1>
+<hr>
+<div class="pengajuan">
+    <div class="table">
+        <div class="table_header">
+            <div>
+
+            </div>
+        </div>
+        <div class="table_section">
+            <table class=" table scroll table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>USERNAME</th>
+                        <th>BARANG</th>
+                        <th>JUMLAH YANG DIAJUKAN</th>
+                        <th>DURASI PEMINJAMAN</th>
+                        <th>TANGGAL PEMINJAMAN</th>
+                        <th>AKSI</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>fathan17</td>
+                        <td>Kamera</td>
+                        <td>1 Unit</td>
+                        <td>2 Hari</td>
+                        <td>22/05/2022</td>
+                        <td>
+                            <button type="button" class="btn btn-success">Setuju</button>
+                            <button type="button" class="btn btn-danger">Tolak</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+@endsection
